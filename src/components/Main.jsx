fe3833687afb2774be0aed8f64b278a626816ff3
ajoshvabi <138970080+ajoshvabi@users.noreply.github.com>
2024-02-02 00:15:44 +0530

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Portfolio } from "./portfolio";
+import "./portfolio.css";
+export const Main = () => {
+  return (
+    <>
+    <Portfolio/>
+    </>
+  );
+};
