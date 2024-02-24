4486e812eabf73dcc9d4381e1bc9161a10c21ed7
ajoshvabi <138970080+ajoshvabi@users.noreply.github.com>
2024-02-24 12:44:13 +0530

    fix

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -243,7 +243,7 @@ export const Portfolio = () => {
                     </Col>
                     <Col className="d-flex  align-items-center justify-content-center px-lg-5 pb-5 pb-lg-0">
                         <Fade direction="up" duration={1000} when={true} cascade>
-                            <img src={bgimg} alt="" className="img-fluid image-with-shadow animate__animated animate__pulse animate__delay-1s animate__slow animate__infinite" />
+                            <img src={bgimg} alt="" className="img-fluid image-with-shadow " />
                         </Fade>
                     </Col>
                 </Row>
