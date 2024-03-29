8b23766bd58e50bf94fafe275e30e41e901fb267
ajoshvabi <138970080+ajoshvabi@users.noreply.github.com>
2024-03-30 00:38:40 +0530

    loading

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -105,6 +105,7 @@ export const Portfolio = () => {
     }, []);
     return (
         <>
+        <Fade duration={2000} triggerOnce={true}>
             {/* navbar */}
             <Navbar expand="lg" className="navbg fixed-top ">
                 <Container >
@@ -854,6 +855,7 @@ export const Portfolio = () => {
                     </a>
                 </span>
             </div>
+            </Fade>
         </>
     );
 };
